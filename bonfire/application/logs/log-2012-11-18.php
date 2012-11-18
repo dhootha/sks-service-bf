f0f365f6b836545d126815cff209f4c5ab4e9b87
Lucky Ong <lucky.wong@gmail.com>
2012-11-19 00:54:39 +0700

    initial commit

---
+++ bonfire/application/logs/log-2012-11-18.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-11-18 16:36:48 --> Query error: No database selected
+ERROR - 2012-11-18 16:38:08 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2012-11-18 16:58:51 --> Severity: Warning  --> opendir(E:\xampp\htdocs\sks\service-bf\bonfire/modules/,E:\xampp\htdocs\sks\service-bf\bonfire/modules/): The system cannot find the path specified. (code: 3) E:\xampp\htdocs\sks\service-bf\sks\codeigniter\helpers\directory_helper.php 46
+ERROR - 2012-11-18 16:58:51 --> Severity: Warning  --> opendir(E:\xampp\htdocs\sks\service-bf\bonfire/modules/): failed to open dir: No such file or directory E:\xampp\htdocs\sks\service-bf\sks\codeigniter\helpers\directory_helper.php 46
+ERROR - 2012-11-18 16:58:51 --> Severity: Warning  --> array_merge(): Argument #2 is not an array E:\xampp\htdocs\sks\service-bf\sks\application\helpers\application_helper.php 163
+ERROR - 2012-11-18 16:58:51 --> Severity: Warning  --> Invalid argument supplied for foreach() E:\xampp\htdocs\sks\service-bf\sks\application\libraries\events.php 78
+ERROR - 2012-11-18 16:58:51 --> Unable to load the requested class: settings_lib
+ERROR - 2012-11-18 16:59:02 --> 404 Page Not Found --> 
+ERROR - 2012-11-18 17:30:18 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
